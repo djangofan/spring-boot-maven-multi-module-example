c660c1bcb34f68fe0943293409479e18651996dc
Jon Austen <djangofan@gmail.com>
2021-02-07 12:10:26 -0800

    Minor edit

--- spring-boot-module/src/main/java/com/example/Application.java
+++ spring-boot-module/src/main/java/com/example/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot2JdbcWithH2Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot2JdbcWithH2Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
